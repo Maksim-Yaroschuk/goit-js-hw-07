a4eed9e99115047dc433f5f82ea70610de358169
Maksim-Yaroschuk <79583144+Maksim-Yaroschuk@users.noreply.github.com>
2022-06-06 22:11:42 +0300

    add task#2 module 7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,7 +4,7 @@ const gallery = document.querySelector(".gallery")
 
 const galleryItem = galleryItems.map(({preview, original, description}) => {
   return `<div class="gallery__item">
-  <a class="gallery__link">
+  <a class="gallery__link" href="${original}">
   <img class="gallery__image" src="${preview}" alt="${description}" data-source="${original}">
   </a>
   </div>`
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery")
+
+const galleryItem = galleryItems.map(({preview, original, description}) => {
+  return `<li><a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}">
+  </a></li>`
+}).join("")
+gallery.insertAdjacentHTML("afterbegin", galleryItem)
+
+
+
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250
+});
+
