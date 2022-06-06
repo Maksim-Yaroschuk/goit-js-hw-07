01c2a19df05fad7027615214d1aa521ac367775b
Maksim-Yaroschuk <79583144+Maksim-Yaroschuk@users.noreply.github.com>
2022-06-06 20:17:55 +0300

    to do task #1, module 7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery")
+
+const galleryItem = galleryItems.map(({preview, original, description}) => {
+  return `<div class="gallery__item">
+  <a class="gallery__link">
+  <img class="gallery__image" src="${preview}" alt="${description}" data-source="${original}">
+  </a>
+  </div>`
+}).join("")
+gallery.insertAdjacentHTML("afterbegin", galleryItem)
+
+gallery.addEventListener("click", onOpenModal);
+
+let instance
+
+function onOpenModal(event) {
+  event.preventDefault()
+  if (event.target.nodeName !== "IMG") {
+    return
+  }
+  instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">`,
+    {
+      onShow: () => window.addEventListener('keyup', onCloseModal),
+      onClose: () => window.removeEventListener('keyup', onCloseModal)
+}
+)
+  instance.show()
+}
+
+
+function onCloseModal(event) {
+  if (event.code === "Escape") {
+    instance.close()
+  }
+}
